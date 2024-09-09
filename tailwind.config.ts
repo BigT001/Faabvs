536d0e7f899e70cff5adeac0713515ca7a36c7e8
BigT001 <unclet1992@gmail.com>
2024-09-10 00:00:32 +0100

    update tailwind.config.ts file. made center: true, a boolean value center: true,

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -75,48 +75,29 @@ const config = {
   					height: '0'
   				}
   			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+			  'gradient-x': {
+				'0%, 100%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'left center',
+				},
+				'50%': {
+				  'background-size': '200% 200%',
+				  'background-position': 'right center',
+				},
+			  },
+			  'drift': {
+				'0%': { transform: 'translate(0, 0)' },
+				'100%': { transform: 'translate(100px, 100px)' },
+			},
   		},
+
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
 			  'gradient-x': 'gradient-x 15s ease infinite',
 			  'drift': 'drift 30s linear infinite',
   		},
-		  keyframes: {
-			'gradient-x': {
-			  '0%, 100%': {
-				'background-size': '200% 200%',
-				'background-position': 'left center',
-			  },
-			  '50%': {
-				'background-size': '200% 200%',
-				'background-position': 'right center',
-			  },
-			},
-			'drift': {
-			  '0%': { transform: 'translate(0, 0)' },
-			  '100%': { transform: 'translate(100px, 100px)' },
-			},
-		  },
-		},
+	},
   },
   plugins: [require("tailwindcss-animate"), ('daisyui')],
 } satisfies Config
