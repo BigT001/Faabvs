89fe692818bae4454a7e47c053a4afd18652ab69
BigT001 <unclet1992@gmail.com>
2024-09-14 02:03:26 +0100

    Updated pricing section

--- app/Pricing/page.tsx
+++ app/Pricing/page.tsx
@@ -16,52 +16,52 @@ interface PricingTier {
   features: string[];
 }
 
-const websitePricingTiers: PricingTier[] = [
-  {
-    name: "Basic",
-    price: "£999",
-    description: "Perfect for small businesses just starting out",
-    features: [
-      "5-page responsive website",
-      "Basic SEO optimization",
-      "Contact form integration",
-      "Mobile-friendly design",
-      "1 round of revisions",
-      "1 month of support",
-    ],
-  },
-  {
-    name: "Professional",
-    price: "£2,499",
-    description: "Ideal for growing businesses with specific needs",
-    features: [
-      "10-page responsive website",
-      "Advanced SEO optimization",
-      "Custom contact form and email integration",
-      "Mobile-friendly design",
-      "E-commerce functionality (up to 50 products)",
-      "Social media integration",
-      "2 rounds of revisions",
-      "3 months of support",
-    ],
-  },
-  {
-    name: "Enterprise",
-    price: "£4,999+",
-    description: "Comprehensive solution for large businesses",
-    features: [
-      "20+ page responsive website",
-      "Comprehensive SEO strategy",
-      "Custom functionality and integrations",
-      "Advanced e-commerce solutions",
-      "User account systems",
-      "Custom admin dashboard",
-      "Performance optimization",
-      "Unlimited revisions",
-      "6 months of support and maintenance",
-    ],
-  },
-];
+// const websitePricingTiers: PricingTier[] = [
+//   {
+//     name: "Basic",
+//     price: "£999",
+//     description: "Perfect for small businesses just starting out",
+//     features: [
+//       "5-page responsive website",
+//       "Basic SEO optimization",
+//       "Contact form integration",
+//       "Mobile-friendly design",
+//       "1 round of revisions",
+//       "1 month of support",
+//     ],
+//   },
+//   {
+//     name: "Professional",
+//     price: "£2,499",
+//     description: "Ideal for growing businesses with specific needs",
+//     features: [
+//       "10-page responsive website",
+//       "Advanced SEO optimization",
+//       "Custom contact form and email integration",
+//       "Mobile-friendly design",
+//       "E-commerce functionality (up to 50 products)",
+//       "Social media integration",
+//       "2 rounds of revisions",
+//       "3 months of support",
+//     ],
+//   },
+//   {
+//     name: "Enterprise",
+//     price: "£4,999+",
+//     description: "Comprehensive solution for large businesses",
+//     features: [
+//       "20+ page responsive website",
+//       "Comprehensive SEO strategy",
+//       "Custom functionality and integrations",
+//       "Advanced e-commerce solutions",
+//       "User account systems",
+//       "Custom admin dashboard",
+//       "Performance optimization",
+//       "Unlimited revisions",
+//       "6 months of support and maintenance",
+//     ],
+//   },
+// ];
 
 const emailPricingTiers: PricingTier[] = [
   {
@@ -78,9 +78,9 @@ const emailPricingTiers: PricingTier[] = [
   {
     name: "Pay as you go £35 per hour ( no additional cost. Only pay for hours used)",
     price: "MONTHLY RETAINER",
-    description: "Minimum of 5hrs per month at £30 per hour. Packages ranges from 5-40hrs per month. ",
+    description:
+      "Minimum of 5hrs per month at £30 per hour. Packages ranges from 5-40hrs per month. ",
     features: [
-      
       "5 hours - £150",
       "10 hours - £300",
       "20 hours - £600",
@@ -118,7 +118,7 @@ const customerServiceTiers: PricingTier[] = [
 
 const socialMediaTiers: PricingTier[] = [
   {
-    name: "Silver",
+    name: "Basic Package ",
     price: "£150/month",
     description: "Basic social media management for one platform",
     features: [
@@ -129,7 +129,7 @@ const socialMediaTiers: PricingTier[] = [
     ],
   },
   {
-    name: "Gold",
+    name: "Standard Package",
     price: "£300/month",
     description: "Enhanced social media presence for one platform",
     features: [
@@ -141,7 +141,7 @@ const socialMediaTiers: PricingTier[] = [
     ],
   },
   {
-    name: "Bronze",
+    name: "Premium Package",
     price: "£450/month",
     description: "Comprehensive social media management for one platform",
     features: [
@@ -157,15 +157,49 @@ const socialMediaTiers: PricingTier[] = [
 
 const minutesTakingTiers: PricingTier[] = [
   {
-    name: "Minutes Taking and Transcription",
-    price: "£30 per hour",
-    description: "Professional minutes taking and transcription services",
+    name: "Basic Package ",
+    price: "£140",
+    description: "",
     features: [
-      "Accurate meeting minutes",
-      "Transcription of audio/video recordings",
-      "Editing and formatting of documents",
-      "Quick turnaround times",
-      "Confidentiality assured",
+      "4 meetings per month",
+      "1 hour meeting length",
+      "Turnaround: Within 24 hours",
+      "Summary of key points",
+      "List of action items with responsible parties",
+    ],
+  },
+
+  {
+    name: "Standard Package",
+    price: "£315",
+    description: "",
+    features: [
+      "6 meetings per month",
+      "Meeting length: 1.5 hours",
+      "Turnaround: Within 48 hours",
+      "Summary of key decisions and action points",
+    ],
+  },
+
+  {
+    name: "Premium Package",
+    price: "£700",
+    description: "",
+    features: [
+      "Up to 10 meetings per month",
+      "Meeting length: 2 hours max",
+      "Turnaround: Within 48hours",
+      "Comprehensive, detailed minutes",
+      "Full summary with decisions, actions, \nand follow-up deadlines",
+    ],
+  },
+
+  {
+    name: "ADD-ON/AD-OC",
+    price: "£35 per hour",
+    description: "",
+    features: [
+      "Delivery 12-24 hours",
     ],
   },
 ];
@@ -189,9 +223,11 @@ const personalAssistantTiers: PricingTier[] = [
 const PricingSection = ({
   title,
   tiers,
+  isMinutesTaking = false,
 }: {
   title: string;
   tiers: PricingTier[];
+  isMinutesTaking?: boolean;
 }) => (
   <div className="mb-32">
     <h2 className="text-3xl font-bold text-center text-secondary mb-8">
@@ -217,9 +253,16 @@ const PricingSection = ({
             <p className="mb-4 text-gray-600">{tier.description}</p>
             <ul className="space-y-2">
               {tier.features.map((feature, featureIndex) => (
-                <li key={featureIndex} className="flex items-center">
-                  <Check className="h-5 w-5 text-primary mr-2" />
-                  <span>{feature}</span>
+                <li key={featureIndex} className="flex items-start">
+                  <Check className="h-5 w-5 text-primary mr-2 mt-1 flex-shrink-0" />
+                  <span>
+                    {feature.split('\n').map((line, lineIndex) => (
+                      <React.Fragment key={lineIndex}>
+                        {line}
+                        {lineIndex < feature.split('\n').length - 1 && <br />}
+                      </React.Fragment>
+                    ))}
+                  </span>
                 </li>
               ))}
             </ul>
@@ -235,9 +278,23 @@ const PricingSection = ({
         </Card>
       ))}
     </div>
+    {isMinutesTaking && (
+      <div className="mt-8 p-6 bg-gray-100 rounded-lg">
+        <h3 className="text-2xl font-bold mb-4">Terms:</h3>
+        <ul className="space-y-2">
+          <li className="flex items-start">
+            <Check className="h-5 w-5 text-primary mr-2 mt-1 flex-shrink-0" />
+            <span>Meetings exceeding time limits will incur an additional fee of £20 per 30 minutes.</span>
+          </li>
+          <li className="flex items-start">
+            <Check className="h-5 w-5 text-primary mr-2 mt-1 flex-shrink-0" />
+            <span>Tools like Minute.me, Google Docs will be used for easier, quicker and efficient deliveries.</span>
+          </li>
+        </ul>
+      </div>
+    )}
   </div>
 );
-
 const ComprehensiveServicesPricing = () => {
   return (
     <div className="min-h-screen bg-gradient-to-r from-purple-50 to-pink-50 relative overflow-hidden">
@@ -262,9 +319,9 @@ const ComprehensiveServicesPricing = () => {
       </div>
 
       <div className="container mx-auto py-12 px-4 relative z-10 text-center">
-        <h1 className="text-5xl font-extrabold text-gray-900  mt-10 relative inline-block">
+        <h1 className="text-5xl font-extrabold text-gray-900  mt-10 mb-20 relative inline-block">
           <span className="relative z-10">My Services</span> <br />
-          <span className="relative z-10 text-primary">  Pricing</span>
+          <span className="relative z-10 text-primary"> Pricing</span>
           <svg
             className="absolute -bottom-2 left-0 w-full h-3 text-primary opacity-30"
             viewBox="0 0 100 10"
@@ -276,129 +333,120 @@ const ComprehensiveServicesPricing = () => {
             ></path>
           </svg>
         </h1>
+
         <PricingSection
-          title="Social Media Management Pricing"
+          title="Social Media Management"
           tiers={socialMediaTiers}
         />
+
         <PricingSection
-          title="Website Development Pricing"
-          tiers={websitePricingTiers}
-        />
-        <PricingSection
-          title="Email Management Pricing"
+          title="Email Management"
           tiers={emailPricingTiers}
         />
         <PricingSection
-          title="Customer Service & Support Pricing"
+          title="Customer Service & Support"
           tiers={customerServiceTiers}
         />
         <PricingSection
-          title="Minutes Taking and Transcription Pricing"
+          title="Minutes Taking and Transcription"
           tiers={minutesTakingTiers}
+          isMinutesTaking={true}
         />
         <PricingSection
-          title="Personal Assistant Pricing"
+          title="Personal Assistant"
           tiers={personalAssistantTiers}
         />
       </div>
 
+      <div className="flex flex-col px-10 lg:px-20 mb-10 md:flex-row gap-4 items-start">
+        <div className="flex-1">
+          <div className="space-y-4">
+            <p className="text-xl">
+              <span className="text-md font-bold text-primary">
+                PLEASE NOTE:{" "}
+              </span>{" "}
+              <br />
+              Utilise the retainer hours within the calendar month of purchase
+              unless specifically pre-agreed, the hours cannot be rolled over. 
+            </p>
+            <ul className="space-y-4">
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">Retainer fees are billed monthly, in advance.</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">Time will be tracked via Toggl.</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">Unused meetings/hours do not roll over to the next month.</span>
+              </li>
+              
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">Payments to be made in advance.</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">
+                  Invoices are raised at the end of the month for hours worked
+                  (50% deposit for new customers for the first month)
+                </span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">
+                  Hours to be used in same month and cannot be rolled over.
+                </span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span className="text-xl">
+                  1 month notice period to terminate by both parties
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
 
-      <div className="flex flex-col px-10 lg:px-20 mb-10 md:flex-row gap-8 items-start">
-      <div className="flex-1">
-        <ul className="space-y-4">
-        <p className="text-xl">
-          <span className="text-md font-bold text-primary">PLEASE NOTE: </span> <br />
-          Utilise the retainer hours within the calendar month of purchase 
-          unless specifically pre-agreed, the hours cannot be rolled over.
-        </p>
-        <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-            Time will be tracked via Toggl.
+        <div className="flex-1 mt-7">
+          <div className="text-xl">
+            <p>
+              Hours are tracked using time-tracking software and I am fully
+              insured and registered with the Information Commissioner's Office
+              (ICO) for Data Protection purposes. That's it! No hidden charges or
+              additional fees. You don't pay for:
             </p>
-          </li>
-
-        <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-            Payments to be made in advance.
+            <ul className="space-y-2 mt-4">
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span>national insurance</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span>taxes</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span>pension</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span>sick pay</span>
+              </li>
+              <li className="flex items-start gap-2">
+                <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
+                <span>or even my lunch break...</span>
+              </li>
+            </ul>
+            <p className="mt-4">
+              all you pay for is my complete, undivided attention when you need
+              it!
             </p>
-          </li>
-
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-              Invoices are raised at the end of the month for hours worked (50% deposit for
-              new customers for the first month)
-            </p>
-          </li>
-          
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-              Hours to be used in same month and cannot be rolled over.
-            </p>
-          </li>
-          
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-              1 month notice period to terminate by both parties
-            </p>
-          </li>
-        </ul>
+          </div>
+        </div>
       </div>
-
-      <div className="flex-1">
-        <p className="text-xl">
-          Hours are tracked using time-tracking software and 
-          I am fully insured and registered with the Information 
-          Commissioner's Office (ICO) for Data Protection purposes.
-          That's it! No hidden charges or additional fees. You don't 
-          pay for  
-          <ul className="space-y-2">
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-              national insurance 
-            </p>
-          </li>
-          
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-             taxes 
-            </p>
-          </li>
-          
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-              pension
-            </p>
-          </li>
-
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-            sick pay
-            </p>
-          </li>
-
-          <li className="flex items-start gap-2">
-            <Check className="flex-shrink-0 w-5 h-5 text-primary mt-1" />
-            <p className="text-xl">
-            or even my lunch break...
-            </p>
-          </li>
-        </ul> 
-           all you pay for is my complete, undivided 
-          attention when you need it!
-        </p>
-      </div>
-      
-      
-    </div>
     </div>
   );
 };
