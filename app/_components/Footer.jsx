76cc2a9ba973cea6e536525f9b6d6acb7a75ac3b
BigT001 <unclet1992@gmail.com>
2024-10-07 16:06:57 +0100

    update footer whatsapp icon

--- app/_components/Footer.jsx
+++ app/_components/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { FacebookIcon, InstagramIcon, TwitterIcon, LinkedinIcon, ArrowRight } from 'lucide-react';
+import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { FaWhatsapp } from 'react-icons/fa';
-
+import { FaInstagram, FaLinkedin, FaTwitter, FaWhatsapp } from 'react-icons/fa';
 
 const navItems = [
   { name: 'Home', path: '/' },
@@ -11,14 +10,14 @@ const navItems = [
   { name: 'Services', path: '/Services' },
   { name: 'Prices', path: '/Pricing' },
   { name: 'Book Me', path: '/BookMe' },
-  { name: 'Privacy Policy',  path: '/Document/PrivacyPolicy.docx.pdf'},
+  { name: 'Privacy Policy', path: '/Document/PrivacyPolicy.docx.pdf'},
 ];
 
 const socialMediaLinks = [
-  { icon: FaWhatsapp, link: "https://wa.me/message/G6BRAVC4QWCAI1" },
-  { name: 'Instagram', icon: InstagramIcon, url: 'https://www.instagram.com/hannah_faab' },
-  { name: 'LinkedIn', icon: LinkedinIcon, url: 'https://www.linkedin.com/in/hannah-olafayo-b093242b3/' },
-  { name: 'Twitter', icon: TwitterIcon, url: 'https://twitter.com/your-profile' },
+  { name: 'Instagram', icon: FaInstagram, url: 'https://www.instagram.com/hannah_faab' },
+  { name: 'LinkedIn', icon: FaLinkedin, url: 'https://www.linkedin.com/in/hannah-olafayo-b093242b3/' },
+  { name: 'Twitter', icon: FaTwitter, url: 'https://twitter.com/your-profile' },
+  { name: 'WhatsApp', icon: FaWhatsapp, url: 'https://wa.me/message/G6BRAVC4QWCAI1' },
 ];
 
 export default function Footer() {
@@ -29,15 +28,13 @@ export default function Footer() {
           <strong className="text-xl text-white sm:text-xl text-center sm:text-left">
             Ready to boost your business productivity?
           </strong>
-          
           <Link href="/BookMe" className="inline-flex items-center gap-2 rounded-full border
-           border-white bg-white px-8 py-3 text-purple-950 hover:bg-transparent hover:text-white
-           focus:outline-none focus:ring active:bg-white/90 transition duration-300">
+            border-white bg-white px-8 py-3 text-purple-950 hover:bg-transparent hover:text-white
+            focus:outline-none focus:ring active:bg-white/90 transition duration-300">
             <span className="text-sm font-medium">Book Me Now</span>
             <ArrowRight className="size-5" />
           </Link>
         </div>
-        
         <nav className="mt-16">
           <ul className="grid gap-4 sm:flex justify-center items-center sm:space-x-8">
             {navItems.map((item) => (
@@ -49,7 +46,6 @@ export default function Footer() {
             ))}
           </ul>
         </nav>
-        
         <div className="mt-8">
           <ul className="flex justify-center gap-6">
             {socialMediaLinks.map((social) => (
@@ -67,7 +63,6 @@ export default function Footer() {
             ))}
           </ul>
         </div>
-        
         <div className="mt-8 flex flex-col items-center">
           <div className="mb-4 flex items-center justify-center overflow-hidden">
             <Image
@@ -79,7 +74,6 @@ export default function Footer() {
             />
           </div>
           <div className="items-center mt-4">
-            
             <p className="text-sm text-gray-600 text-center">
               Copyright &copy; FAABvs {new Date().getFullYear()}. All rights reserved.
             </p>
